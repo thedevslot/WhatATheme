e5f4c58bafdb519fdadbadbc7fbdccf5d32984c9
fabon <syobon.hinata.public@gmail.com>
2022-08-01 11:30:59 +0900

    Fix quote style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 (function() {
-    if (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0) {
+    if (document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0) {
         // webp supported
         Array.prototype.forEach.call(document.querySelectorAll("a[data-lightbox-webp]"), function(node) {
             node.href = node.dataset.lightboxWebp;
